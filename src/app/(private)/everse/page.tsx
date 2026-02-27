5ff6d6c442c53f88f415ace981a35ba9362a8e54
Nitesh Malviya <nitesh@bitcot.com>
2026-02-27 18:48:59 +0530

    everse home compoenent create

---
+++ src/app/(private)/everse/page.tsx
@@ -0,0 +1,9 @@
+import EverseHome from "@/components/everse";
+
+const eversePage = () => {
+    return (
+        <EverseHome />
+    )
+}
+
+export default eversePage;
---
+++ src/components/everse/index.tsx
@@ -0,0 +1,8 @@
+const EverseHome = () => {
+    return (
+        <div>Everse Home</div>
+    )
+}
+
+
+export default EverseHome;
